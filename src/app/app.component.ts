8e5ab4b33f967d749e60da09a4d8873162a1c5b8
ForeveYang <hrrragon@gmail.com>
2021-04-28 15:55:55 +0300

    added styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,28 @@ import { WeatherService } from './weather.service';
 })
 export class AppComponent {
   data:any;
+  error:any;
+  visible: boolean = false
 
   constructor(private http:WeatherService) { }
-  showWeather(){
-    this.http.getData().subscribe(
+  showWeather(city:any){
+    this.http.getData(city).subscribe(
       (data:any) => {
         this.data = data;
-        console.log(data);
-        
+        data.main.temp = (Math.round(data.main.temp - 273.15));
+        data.main.feels_like = (Math.round(data.main.feels_like - 273.15));
+        if (data.main.temp > 0){
+          data.main.temp = '+' + data.main.temp;
+        }
+        if (data.main.feels_like > 0){
+          data.main.feels_like = '+' + data.main.feels_like;
+        }
+        this.visible = true;
+
+      },
+      (error:any) => {
+        this.error = error;
+        alert('Invalid name of city. Please, check the city name')
       }
     )
   }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -8,9 +8,9 @@ import { HttpClient } from '@angular/common/http'
 export class WeatherService {
 
   constructor(private http:HttpClient) { }
-
-  getData():Observable<any>{
-    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=35a0f6738c93c3c929ec30bbb3f53f1a')
+  
+  getData(city:string):Observable<any>{
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=35a0f6738c93c3c929ec30bbb3f53f1a')
   }
 
 }
