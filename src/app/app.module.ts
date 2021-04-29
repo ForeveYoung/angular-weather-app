dffbdefc0761730427bb3824075b92545ad0795b
ForeveYang <hrrragon@gmail.com>
2021-04-29 23:57:30 +0300

    added reactive form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WeatherService } from './weather.service';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
