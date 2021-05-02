c457230d6f6f036e3dda88757681b9adebd4b8e4
ForeveYang <hrrragon@gmail.com>
2021-05-02 12:26:30 +0300

    changed http to https

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -10,7 +10,7 @@ export class WeatherService {
   constructor(private http:HttpClient) { }
   
   getData(city:string):Observable<any>{
-    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=35a0f6738c93c3c929ec30bbb3f53f1a')
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=35a0f6738c93c3c929ec30bbb3f53f1a')
   }
 
 }
